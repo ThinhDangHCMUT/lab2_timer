9a855577e759f24aa2476d87c4478dfbebcd2c0a
unknown <danggiathinhgocong@gmail.com>
2022-09-28 23:16:15 +0700

    name enabel pin

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -60,6 +60,10 @@ void Error_Handler(void);
 /* Private defines -----------------------------------------------------------*/
 #define LED_Pin GPIO_PIN_5
 #define LED_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_6
+#define EN0_GPIO_Port GPIOA
+#define EN1_Pin GPIO_PIN_7
+#define EN1_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -193,14 +193,14 @@ int main(void)
 		  setTimer1(50);
 		  switch(status){
 		  			case 0:
-		  				HAL_GPIO_WritePin( GPIOA , GPIO_PIN_6 , SET);
-		  				HAL_GPIO_WritePin( GPIOA , GPIO_PIN_7 , RESET);
+		  				HAL_GPIO_WritePin( GPIOA , EN0_Pin , SET);
+		  				HAL_GPIO_WritePin( GPIOA , EN1_Pin , RESET);
 		  				SevenSegment_Update(1);
 		  				status = 1;
 		  				break;
 		  			case 1:
-		  				HAL_GPIO_WritePin( GPIOA , GPIO_PIN_6 , RESET);
-		  				HAL_GPIO_WritePin( GPIOA , GPIO_PIN_7 , SET);
+		  				HAL_GPIO_WritePin( GPIOA , EN0_Pin , RESET);
+		  				HAL_GPIO_WritePin( GPIOA , EN1_Pin , SET);
 		  				SevenSegment_Update(4);
 		  				status = 0;
 		  				break;
@@ -308,14 +308,14 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_Pin|GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_Pin|EN0_Pin|EN1_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
                           |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_Pin PA6 PA7 */
-  GPIO_InitStruct.Pin = LED_Pin|GPIO_PIN_6|GPIO_PIN_7;
+  /*Configure GPIO pins : LED_Pin EN0_Pin EN1_Pin */
+  GPIO_InitStruct.Pin = LED_Pin|EN0_Pin|EN1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
